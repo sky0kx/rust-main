c55543d4d44f9a8e070d43477b302a0b3602c604
Sky <93914899+sky0kx@users.noreply.github.com>
2025-05-12 02:18:18 +0300

    Update hw03.rs

--- homework/hw03.rs
+++ homework/hw03.rs
@@ -1,1 +1,24 @@
+const WIDTH: usize = 30;
+const HEIGHT: usize = 14;
 
+pub fn draw_envelope() {
+    let mut output = String::new();
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let c = if y == 0 || y == HEIGHT - 1 {
+                '*'
+            } else if x == 0 || x == WIDTH - 1 {
+                '*'
+            } else if x == y || x == WIDTH - 1 - y {
+                '*'
+            } else {
+                ' '
+            };
+            output.push(c);
+        }
+        output.push('\n');
+    }
+
+    print!("{}", output);
+}
